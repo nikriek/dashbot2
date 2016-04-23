c9b74d83f0dae789104172e88568b16476bb0745
Niklas Riekenbrauck <nikriek@gmail.com>
2016-04-23 15:53:11 +0100

    add missing scope

--- auth/slack.js
+++ auth/slack.js
@@ -6,7 +6,7 @@ passport.use(new SlackStrategy({
         clientID: config.slack.clientID,
         clientSecret: config.slack.clientSecret,
         callbackURL: config.slack.redirectUri,
-        scope: 'identify channels:read chat:write:user'
+        scope: 'identify channels:read chat:write:user users:read'
     }, function(accessToken, refreshToken, profile, done) {
 
     })
