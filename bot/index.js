30099bcc679ce2d9b7dabe2c819c1c11cd7a5d0c
NicoKnoll <mail@nico.is>
2016-04-24 01:09:49 +0100

    twitch

--- bot/index.js
+++ bot/index.js
@@ -265,3 +265,27 @@ function configureYoutube(controller, websocketServer) {
     });
 }
 
+
+function configureYoutube(controller, websocketServer) {
+    controller.hears('youtube (\\w+)', 'direct_message,direct_mention,mention', function(bot, message) {
+        channel = message.match[1]
+        var payload = JSON.stringify({
+            type: 'twitch',
+            data: {
+                url: 'https://player.twitch.tv/?channel=' + channel
+            },
+            col:'1',
+            row:'1',
+            sizex:'3',
+            sizey:'2'
+        });
+        websocketServer.clients.forEach(function(client) {
+            client.send(payload);
+        });
+        bot.reply(message, getReply());
+    });
+}
+
+
+
+
